cb0af2fc3a0f1f1d0fc83cd9c292d3463a1ea4fb
Brsalsabil <sabilbr90@gmail.com>
2023-07-01 10:09:50 -0700

    4-puts.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A programt that prints the size of various types on the computer
+ * Return: (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf(Size of a char: 
