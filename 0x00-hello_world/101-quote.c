7ae7e4fdeb2257aee6674f8baa77e30aa7b3b85c
Brsalsabil <sabilbr90@gmail.com>
2023-07-02 00:40:40 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - A C program that prints a line followed by the  standard error
  *
- * Return: 1 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (0);
 }
