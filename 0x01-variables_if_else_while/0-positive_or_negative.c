d351dbd590a651aee9fcbd74ae281c8ebd03159e
Brsalsabil <sabilbr90@gmail.com>
2023-07-02 02:03:40 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,26 +5,21 @@
  * main - print whether the number stored in the variable n is positive, zero or negative
  * Return: Always 0 (success)
  */
-
 int main(void)
-{
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 if ( n>0 )
-{ 
+{
 	print("%d is positive\n", n);
 }
-	else if ( n == o) 
+	else if ( n == o)
 {
 	print("%d is zero\n", n);
 }
-else 
+else
 {
 	print("%d is negtative\n", n);
-
 }
-
 	return (0);
 }
