85d7a12a6a45ecab99dcd080536aebd213fe9d36
Brsalsabil <sabilbr90@gmail.com>
2023-07-04 12:06:38 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,7 +6,6 @@
 int maint(void)
 {
 	char c;
-
 	int d;
 
 	c = 'a';
@@ -15,6 +14,11 @@ int maint(void)
 		(d < 10) {
 				putchar(d + '0');
 				d++;
+		}
+	while
+		(c <= 'f') {
+				putchar(c);
+				c++;
 		}
 	putchar ('\n');
 	return (0);
